86344cdb1d3716176e43b3e5cac7786cdf40e42e
Kaoru Kohashigawa <dev@kaoruk.com>
2016-04-14 07:46:46 -0700

    Adds specs to ensure a rails application has access to include_media.scss, also setups travis-ci

--- spec/include_media_rails_spec.rb
+++ spec/include_media_rails_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe IncludeMediaRails do
+  let (:dummy_app) { Dummy::Application }
+
   it 'has a version number' do
     expect(IncludeMediaRails::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'will plugin include_media.scss' do
+    expect(dummy_app.assets['include_media']).not_to be_nil
   end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("./dummy/config/environment.rb",  __dir__)
+
 require 'include_media_rails'
